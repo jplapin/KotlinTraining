14f8d93becf638130e976ad81069754a0b67c532
jplapin <jp.da.costa.coelho@gmail.com>
2016-11-26 12:15:07 +0000

    First Kotlin Commit

---
+++ HelloWorld.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by João on 26/11/2016.
+ */
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
